5885d3307c8b7da2ca069c2dd1589fad6cff727e
jmorgan1321 <jeffreymorgan1321@gmail.com>
2014-12-06 17:17:02 -0800

    got some of the webstuff back up and working.  almost there!

--- v1/router.go
+++ v1/router.go
@@ -1,14 +1,14 @@
 package main
 
-// import (
-// 	"github.com/gorilla/mux"
-// )
+import (
+	"github.com/gorilla/mux"
+)
 
-// func NewRouter() *mux.Router {
-// 	router := mux.NewRouter().StrictSlash(true)
-// 	for _, r := range routes {
-// 		h := Logger(r.Handler, r.Name)
-// 		router.Methods(r.Method).Path(r.Pattern).Name(r.Name).Handler(h)
-// 	}
-// 	return router
-// }
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, r := range routes {
+		h := Logger(r.Handler, r.Name)
+		router.Methods(r.Method).Path(r.Pattern).Name(r.Name).Handler(h)
+	}
+	return router
+}
--- v1/routes.go
+++ v1/routes.go
@@ -1,43 +1,43 @@
 package main
 
-// import (
-// 	"net/http"
-// )
+import (
+	"net/http"
+)
 
-// type Route struct {
-// 	Name, Method, Pattern string
-// 	Handler               http.HandlerFunc
-// }
+type Route struct {
+	Name, Method, Pattern string
+	Handler               http.HandlerFunc
+}
 
-// type Routes []Routes
+type Routes []Routes
 
-// const (
-// 	apiPrefix = "/api/v1"
-// )
+const (
+	apiPrefix = "/api/v1"
+)
 
-// var routes = []Route{
-// 	Route{
-// 		Name:    "Index",
-// 		Method:  "GET",
-// 		Pattern: "/",
-// 		Handler: nil,
-// 	},
-// 	Route{
-// 		Name:    "CardIndex",
-// 		Method:  "GET",
-// 		Pattern: apiPrefix + "/cards",
-// 		Handler: cardIndexHandler,
-// 	},
-// 	Route{
-// 		Name:    "Review",
-// 		Method:  "GET",
-// 		Pattern: apiPrefix + "/review/{value}",
-// 		Handler: reviewHandler,
-// 	},
-// 	Route{
-// 		Name:    "Save",
-// 		Method:  "GET",
-// 		Pattern: apiPrefix + "/save",
-// 		Handler: saveHandler,
-// 	},
-// }
+var routes = []Route{
+	// Route{
+	// 	Name:    "Index",
+	// 	Method:  "GET",
+	// 	Pattern: "/",
+	// 	Handler: http.FileServer(http.Dir(`C:\Users\jmorgan\Sandbox\golang\src\github.com\jmorgan1321\SpaceRep\v1\html\static`)),
+	// },
+	Route{
+		Name:    "CardIndex",
+		Method:  "GET",
+		Pattern: apiPrefix + "/cards",
+		Handler: cardIndexHandler,
+	},
+	Route{
+		Name:    "Review",
+		Method:  "GET",
+		Pattern: apiPrefix + "/review/{value}",
+		Handler: reviewHandler,
+	},
+	Route{
+		Name:    "Save",
+		Method:  "GET",
+		Pattern: apiPrefix + "/save",
+		Handler: saveHandler,
+	},
+}
